fbf2308cf5d5c79c64da0ddc7f28e705fe11c23f
Yashodhara-123 <sandaniyashodhara10@gmail.com>
2025-06-04 00:11:23 +0530

    Add files via upload

---
+++ Day 1/Sorting/Bubble Sort/bubble_sort.m
@@ -0,0 +1,18 @@
+
+arr = [64,34,25,12,22,11,90];
+
+n = length(arr);
+
+for i = 1:n-1
+    for j = 1:n-1
+        if arr(j) > arr(j+1)
+            % Swap elements
+            temp = arr(j);
+            arr(j) = arr(j+1);
+            arr(j+1) = temp;
+        end
+    end
+end
+
+disp('Sorted array:')
+disp(arr)
